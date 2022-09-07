35784f63714c646ec6fa321160ba8e91129a2e88
ShinMyeongJi <shinmj1207@gmail.com>
2022-09-07 22:57:12 +0900

    init commit

---
+++ gateway/src/main/java/com/shinmj/msa/security/SpringGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.shinmj.msa.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * packageName :  com.shinmj.msa.security
+ * fileName : SpringGatewayApplication
+ * author :  home
+ * date : 22. 9. 7.
+ * description :
+ */
+@SpringBootApplication
+public class SpringGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class);
+    }
+}
